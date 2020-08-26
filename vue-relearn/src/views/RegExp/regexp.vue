844c3757c8890425721e8b352fc486ebaacee2c6
wanghu <wanghu@xiaoxiangyoupin.cn>
2020-08-26 15:04:41 +0800

    m

--- vue-relearn/src/views/RegExp/regexp.vue
+++ vue-relearn/src/views/RegExp/regexp.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-      <button @click="m1">小数</button>
+      <van-button type="primary" @click="m1">小数</van-button>
+      <van-button type="primary" @click="m2">版本号</van-button>
+      <van-button type="primary" @click="m3">不包含字母</van-button>
   </div>
 </template>
 
@@ -9,9 +11,22 @@ export default {
     methods:{
         m1(){
           let reg=/^\d+\.\d+$/
-          let digit=0.78
+          let digit=7.8
           console.log(reg.test(digit))  
-        }
+        },
+        m2(){
+        // 符号 ?: 非捕获元之一
+          let reg=/^\d+(?:\.\d+){2}$/   
+          let version='17.82.5'
+          console.log(reg.test(version))  
+        },
+        m3(){
+            // 匹配任何包含零个或多个 n 的字符串---- n*
+          let reg=/^[^A-Za-z]*$/ 
+          let str='17.82.5'
+          console.log(reg.test(str))  
+        },
+        
     },
     mounted(){
     }
